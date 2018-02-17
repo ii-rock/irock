484eed26138e4c9063e380b52f89dab5df0ddef5
ii-rock <35752422+ii-rock@users.noreply.github.com>
2018-02-17 13:43:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,11 +164,7 @@ bot.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
     
     if (!message.content.startsWith(prefix)) return;
-    if (message.guild.id = "314192812507791362") {
-	    
-    } else {
-	    bot.channels.get("405872224806109185").sendMessage(`${message.author.username}#${message.author.discriminator}: ${message.content}`);
-    }
+    if (!message.guild.id === "314192812507791362") bot.channels.get("405872224806109185").sendMessage(`${message.author.username}#${message.author.discriminator}: ${message.content}`);
 
     let WholeMsg = message.content.split(" ").slice(1)
     let theMsg = WholeMsg.join(" ")
