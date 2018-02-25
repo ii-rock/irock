aea393fb6545b7026b2d88d92fc8c8a86918ff1f
ii-rock <35752422+ii-rock@users.noreply.github.com>
2018-02-25 12:29:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ bot.on('guildMemberAdd', member => {
       if (!channel) return;
   }
 
-      member.user.send(`Whazzup ${member.user.username}, hope you enjoy your time in **${member.guild.name}**!`)
+      member.user.send(`:raised_hands: Whazzup ${member.user.username}, hope you enjoy your time in **${member.guild.name}**!`)
   
 });
 
@@ -200,6 +200,12 @@ bot.on("message", function(message) {
     var args = message.content.substring(prefix.length).split(" ");
 
     switch (args[0].toLowerCase()) {
+	    case "dm":
+		    if (!mentioned) return m.send('Comon, that\'s not possible, you have to mention a user!')
+		   if (!theMsg.replace(mentioned, '')) (':poop: Discord does not allow empty messages...')
+		    mentioned.send(`${author.username}#${author.discriminator} told me to send you a direct message :slight_smile:\n:speech_balloon: ${theMsg.replace(mentioned, '')}`)
+                   
+	    break;
 	    case "yomama":
 	    if (!mentioned) return m.send(':warning: You have to mention a user!')
 	    var joke = jokes[Math.floor(Math.random() * jokes.length)]
@@ -374,7 +380,7 @@ bot.on("message", function(message) {
 	        .addField("Moderation", `${prefix}userinfo \`<user>\` - shows a few information about the mentioned user.\n${prefix}serverinfo - shows a few information about the current guild.\n${prefix}getinvite - creates an invite for the current or mentioned channel.\n${prefix}settopic \`<mention a channel> <new topic>\` - changes the current or mentioned channel's topic.\n${prefix}purge \`<number of messages (1-100)>\` - deletes a specified amount of messages.\n${prefix}ban \`<user> <reason>\` - bans a user from the server.\n${prefix}kick \`<user> <reason>\` - kicks a user from the server.\n${prefix}report \`<user> <reason>\` - report a user with a reason and it will be sent in the **reports** channel if found.`)
                 .addField("Google", `${prefix}google \`<search query>\` - search something on google and the bot will give you the link.\n${prefix}shortenurl \`<URL/Link>\` - convert a long link to a short one.\n${prefix}image \`<search query>\` - search for an image on google.`)
                 .addField("Cleverbot System", `${prefix}talk \`<message>\` - talk to the bot and it will reply to you.\n(Direct Messaging): You can chat with the bot privately and it will reply to you asap!\nExample,\nUser: Hey\n${bot.user.username}: Hey, how are you?`)
-	        .addField("Other", `${prefix}8ball - ask a question and the bot will reply with a random answer.\n${prefix}say \`<message>\` - says your message.\n${prefix}cat - sends a random cat picture.\n${prefix}dog - sends a random dog picture.\n${prefix}roll - rolls a number.\n${prefix}yomama \`<user>\` - Joke with the mentioned user using yomama jokes.`)
+	        .addField("Other", `${prefix}8ball - ask a question and the bot will reply with a random answer.\n${prefix}say \`<message>\` - says your message.\n${prefix}cat - sends a random cat picture.\n${prefix}dog - sends a random dog picture.\n${prefix}roll - rolls a number.\n${prefix}yomama \`<user>\` - Joke with the mentioned user using yomama jokes.\n${prefix}dm \`<user> <message>\` - send the mentioned user a direct message.`)
                 .addField("About Bot", `${prefix}ping - shows the time taken for the bot to respond.\n${prefix}uptime - shows the time since the bot has started up.\n${prefix}servers - shows the servers count that the bot has joined.\n${prefix}about - shows information about the bot's owner and the library used to create the bot.\n${prefix}invite - sends my invitation link.\n${prefix}reportbug - report a bug and it will be sent to the owner.`)
                 .setColor("#3C51C3")
                 .setFooter(`Requested by ${message.author.username}#${author.discriminator}`, message.author.displayAvatarURL)
