dbe06b289ca32fc57811a1e1bb3109e8cd1e076b
ii-rock <35752422+ii-rock@users.noreply.github.com>
2018-02-25 18:01:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -192,7 +192,7 @@ bot.on("message", function(message) {
 	        .addField("Other", `${prefix}8ball - ask a question and the bot will reply with a random answer.\n${prefix}say \`<message>\` - says your message.\n${prefix}cat - sends a random cat picture.\n${prefix}dog - sends a random dog picture.\n${prefix}roll - rolls a number.\n${prefix}yomama \`<user>\` - Joke with the mentioned user using yomama jokes.\n${prefix}dm \`<user> <message>\` - send the mentioned user a direct message.`)
                 .addField("About Bot", `${prefix}ping - shows the time taken for the bot to respond.\n${prefix}uptime - shows the time since the bot has started up.\n${prefix}servers - shows the servers count that the bot has joined.\n${prefix}about - shows information about the bot's owner and the library used to create the bot.\n${prefix}invite - sends my invitation link.\n${prefix}reportbug - report a bug and it will be sent to the owner.`)
                 .setColor("#3C51C3")
-                .setFooter(`Requested by ${message.author.username}#${author.discriminator}`, message.author.displayAvatarURL)
+                .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.author.displayAvatarURL)
                 .setTimestamp()
 	if (!message.guild) {
         bot.channels.get("405872224806109185").sendMessage(`[Private] ${message.author.username}#${message.author.discriminator}: ${message.content}`);
@@ -202,12 +202,11 @@ bot.on("message", function(message) {
        bot.channels.get("405872224806109185").sendMessage(`[Reply] ${bot.user.username}#${bot.user.discriminator}: ${response.output}`);
 	    
     })
-	} else if (!message.content.includes('help'))
+	} else if (!message.content.includes('help')) {
 		message.author.send({embed})
 		message.author.send(`I am a music bot, clever.\nInvite me to your guild from [Here](${process.env.invite})`)
 	}
-	
-})
+}
     var mentioned = message.mentions.users.first()
     var channels = message.mentions.channels.first()
     var m = message.channel
